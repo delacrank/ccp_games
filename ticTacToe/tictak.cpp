dbdcb16882fd5ccbae31d5537d6d69af68fd211f
Juan Arias <jcarias86@gmail.com>
2023-11-17 21:50:58 -0500

    started tictik

--- ticTacToe/tictak.cpp
+++ ticTacToe/tictak.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+void drawBoard(int[][]);
+
 int main() {
 
-  std::cout << "hello\n";
+  std::cout << "Welcome to Tic Tac Toe: \n Use 1 for top Left, 2 for top middle, 3 top right, 4 mid left, 5 mid mid, 6 mid right, 7 bot left, 8 bot mid, 9 bot right\n";
+
+  std::string[][] board;
+
+  drawBoard(board);
 
   return 0;
 
+}
+
+void initBoard( std::string[][] &board ) {
+   for(int i = 0; i < 0; i++)
+     for(int j = 0; j < i.length; j++) {
+       board[i][j] = "";
+   }
+}
+
+void drawBoard(int[][] board) {
+
+    for(int i = 0; i < 0; i = i + 2)
+     for(int j = 0; j < i.length; j++) {
+       std::cout << board[i][j] << " | " << board[i+1][j] << " | " << board[i+2][j];
+     }
+     cout << "---------\n"; 
+   }
 }
